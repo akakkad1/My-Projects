061bec8ff41532e62a0bcb7670126afc40f79329
Adit Kakkad <aditkpython@gmail.com>
2026-04-15 14:05:45 +0000

    Final Submission

--- school/apcsp/mockcreate/mad_libs.py
+++ school/apcsp/mockcreate/mad_libs.py
@@ -7,13 +7,13 @@ set1 = ["Noun", "Adjective", "Verb", "Place", "Exclamation"]
 set2 = ["Adjective", "Noun", "Verb", "Adverb", "Food", "Color", "Animal", "Noun", "Adjective", "Noun"]
 set3 = ["Noun", "Food", "Plural Noun", "Plural Noun", "Plural Noun", "Adjective", "Nonsense Word", "Adjective", "Noun", "Plural Noun", "Part Of Body", "Person", "Animal", "Plural Noun", "Body Part", "Plural Noun", "Adverb", "Plural Noun", "Noun", "Verb", "Adjective", "Place", "Color", "Noun", "Adjective", "Animal", "Verb", "Number", "Noun", "Noun"]
 
+sets = [set1, set2, set3]
+
 phrases = [
     "The #1 was #2. #3 at #4? #5!",
     "A #1 #2 decided to #3 #4. It ate #5 and saw a #6 #7. What a #8 #9 #10!",
     "During the #1, people threw #2. The #3 hit #4 and #5. It was #6! I shouted #7. A #8 #9 saw #10. My #11 felt #12. The #13 escaped #14. My #15 had #16. We ran #17 toward #18. The #19 began to #20. It was #21 at #22. A #23 #24 looked #25. That #26 started to #27. We saw #28 #29 by the #30."
 ]
-
-sets = [set1, set2, set3]
 
 # Finds out how long of a phrase the user wants
 print("Welcome to Mad Libs!!!")
@@ -24,6 +24,7 @@ Pick a set:
 3 - Long | {len(set3)} Words
 Selected Set: """))
 
+# Tells the user what choice they picked
 os.system('clear')
 print(f"You picked set: {choice}\n")
 
@@ -41,11 +42,9 @@ def fill_words(selected_list):
     user_set = []
     for word_type in selected_list:
         user_word = input(f"Enter a {word_type}: ")
-        
         if len(user_word) < 1:
             print("You did not enter a word. Defaulting to empty_space.")
             user_word = "empty_space"
-            
         user_set.append(user_word)
     return user_set
 
@@ -60,7 +59,7 @@ for i in range(len(new_words), 0, -1):
     placeholder = f"#{i}"
     story = story.replace(placeholder, new_words[i-1]) 
 
-
+# Displays the user-inputted words and final story
 os.system('clear')
 your_words = ", ".join(new_words)
 print(f"Your Words: {your_words}\n")
