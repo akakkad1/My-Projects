aede13244ad3d9297a8caf438e251ae5043d9ad3
Adit Kakkad <aditkpython@gmail.com>
2026-04-23 13:38:28 +0000

    first draft

--- personal/musicfinder/main.py
+++ personal/musicfinder/main.py
@@ -1,9 +1,21 @@
 from google import genai
+import os
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="AIzaSyDaQk614C6Wwn38L3hM0PXSgdL_Wp_vkbY")
+client = genai.Client(api_key="")
 
-picks = ["pop","2020s","happy"]
+picks = []
+os.system('clear')
+surprise = input("Surprise me? (y/n): ")
+if surprise.lower() == "y":
+    picks.append("\nSurprise me!")
+elif surprise.lower() == "n":
+    genre = input("\nWhat type of genre of music do you want?: ")
+    mood = input("What mood of music do you want?: ")
+    other = input("Anything else? (ex. artist, language): ")
+    picks += [genre, mood, other]
+
+
 response = client.models.generate_content(
     model="gemini-3-flash-preview", contents=f"Give me a song in this format Song Name - Artist Name, based on these info {picks}"
 )
