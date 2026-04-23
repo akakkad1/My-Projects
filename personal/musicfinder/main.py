323f34b31dbc11a6446389bf881d7484f155409b
Adit Kakkad <aditkpython@gmail.com>
2026-04-23 13:03:09 +0000

    new project for fun- music finder

--- personal/musicfinder/main.py
+++ personal/musicfinder/main.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client()
+client = genai.Client(api_key="AIzaSyDaQk614C6Wwn38L3hM0PXSgdL_Wp_vkbY")
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
