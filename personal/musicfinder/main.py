79a8ae167f7b2da2a399510986236015d94f6771
Adit Kakkad <aditkpython@gmail.com>
2026-04-23 13:10:00 +0000

    inital ai testing

--- personal/musicfinder/main.py
+++ personal/musicfinder/main.py
@@ -3,7 +3,8 @@ from google import genai
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client(api_key="AIzaSyDaQk614C6Wwn38L3hM0PXSgdL_Wp_vkbY")
 
+picks = ["pop","2020s","happy"]
 response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+    model="gemini-3-flash-preview", contents=f"Give me a song in this format Song Name - Artist Name, based on these info {picks}"
 )
 print(response.text)
